bf0aab1ebf475730e35e84817e318c8b3552cfa3
Viachaslau Kitsun <Viachaslau_Kitsun@epam.com>
2019-03-04 09:21:11 +0300

    add some unit tests

---
+++ FileListener.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FileListener.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("FileListener.Tests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("924eacf9-6e39-40d7-94db-b8e35040963c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FileListener.Tests/Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using SorterServiceConfiguration;
+using FileListener;
+
+namespace FileSystemSorter.Tests
+{
+    [TestClass]
+    public class FileSystemSorterTests
+    {
+        private IFileSystemWorker fileSystemInfoProviderMock;
+        private IDirectoryWorker directoryWatcherMock;
+        private FileListener.FileListener fileListener;
+        private List<string> existingDirectoryPaths;
+        private List<string> filesToAddNames;
+
+        private readonly string directoryToWatchPath = "I://test";
+        private readonly string firstLevelFolderName = "data";
+        private readonly string defaultPathToMove = "default";
+
+        [TestInitialize]
+        public void Init()
+        {
+                     
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileListener_ListedDirectoryIsNull_ThrowsException()
+        {
+            fileListener= new FileListener.FileListener(null, existingDirectoryPaths[1], fileSystemInfoProviderMock);
+            
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileSystemSorter_DefaultDirectoryIsNull_ThrowsException()
+        {
+            fileListener = new FileListener.FileListener(directoryWatcherMock, null, fileSystemInfoProviderMock);
+        }
+
+
+
+
+        [TestMethod]
+        public void FileListener_Correct_MoveFile()
+        {
+           
+        }
+    }
+}
